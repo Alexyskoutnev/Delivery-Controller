8dceeb6d1dc04dcb375cbe351d5f73d290856cb2
alexyskoutnev <alexyskoutnev@gmail.com>
2023-11-09 13:51:19 -0600

    init with env setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='RL_Pizza_Delivery',
+    version='0.0.1',
+    packages=find_packages(),  # This will automatically discover and include all packages
+    install_requires=[
+        # List your dependencies here
+        'numpy',
+        'matplotlib',
+    ],
+    author='Alexy Skoutnev, Oluwatito Ebiwonjumi',
+    author_email='alexy.a.skoutnev@vanderbilt.edu',
+    description='Pizza Delivery planning controller with safety properties',
+)
