6a2e90c2aedbdf8cbc1dbda4514d822ab10b9acd
alexyskoutnev <alexyskoutnev@gmail.com>
2023-11-27 13:23:50 -0600

    fixed video recording

--- RL_Pizza_Delivery/utils/torch_utils.py
+++ RL_Pizza_Delivery/utils/torch_utils.py
@@ -5,9 +5,10 @@ import datetime
 import torch
 import torch.nn as nn
 import numpy as np
+import imageio
 
 SAVE_DIR = "../data/models"
-SAVE_VID_DIR = "../data/vids"
+SAVE_VID_DIR = "../data/videos"
 
 def save_model(model):
     date = datetime.datetime.now()
@@ -17,12 +18,6 @@ def save_model(model):
 
 def save_frames(buffer):
     date = datetime.datetime.now()
-    save_path = os.path.join(SAVE_VID_DIR, "DQN-VID-" + str(date) + ".mp4")
-    size = 512, 512
-    fps = 25
-    out = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (size[1], size[0]), False)
-    for img in np.array(buffer, dtype='uint8'):
-        img = np.moveaxis(img, -1, 0)
-        out.write(img)
-    out.release()
+    save_path = os.path.join(SAVE_VID_DIR, "DQN-VID-" + str(date) + ".gif")
+    imageio.mimsave(save_path, buffer)
 
--- scripts/train.py
+++ scripts/train.py
@@ -48,15 +48,20 @@ def train(agent, env, buffer, writer=None, config=None):
         if epoch % config['EVAL_ITR'] == 0:
             state = env.reset()
             buf = list()
+            eval_total_rewards = 0
             for i in range(100):
                 if config['render_mode'] == 'rgb_array':
                     buf.append(env.render())
                 action = agent.select_action(torch.tensor(state, dtype=torch.float32).view(1, -1), espilon=0)
                 next_state, reward, done, _ = env.step(action)
                 state = next_state
+                eval_total_rewards += reward
                 if done:
                     break
-            save_frames(buf)
+            print("====================== EVALUALTION ======================")
+            print(f"[{epoch} / {config['epochs']}] : reward [{eval_total_rewards:.3f}] \t")
+            if config['record_vid']:
+                save_frames(buf)
         batch = buffer.sample(config['BATCH_SIZE'])
         loss = agent.update(batch)
         writer.add_scalar("loss", loss, frame_idx)
@@ -67,7 +72,7 @@ if __name__ == "__main__":
     config = {"map_size": (5, 5), "potholes" : 0, "traffic_jams": 0, "render_mode": 'rgb_array',
               "GAMMA" : 0.99, "BATCH_SIZE": 32, "REPLAY_SIZE": 10000, "LR": 1e-4, "SYNC_TARGET_FRAMES": 1000,
               "REPLAY_START_SIZE": 10000, "EPSILON_DECAY_LAST_FRAME": 100000, "EPSILON_START": 1.0, "EPSILON_FINAL": 0.01,
-              "epochs" : 100000, "EVAL_ITR": 1000}
+              "epochs" : 100000, "EVAL_ITR": 10000, "record_vid" : True}
     env = ENV_OBSTACLE(map_size=config['map_size'], render_mode=config['render_mode'], potholes=config['potholes'], traffic_jams=config['traffic_jams'])
     buffer = ExperienceBuffer(config['REPLAY_SIZE'])
     agent = QAgent(env, buffer, lr=config['LR'], gamma=config['GAMMA'])
